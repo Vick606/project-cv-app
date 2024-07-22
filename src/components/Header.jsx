3afa311443d9307cb11f1da4c67e32f19f8f7c8f
Victor Koech <vickoch20@gmail.com>
2024-07-22 17:39:37 +0300

    Update Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/Header.css'
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>CV Generator</h1>
+    </header>
+  )
+}
+
+export default Header
